6143341fdab315ef29473f4dc4fe0f0bd630f4d3
cuberisu <cuberisu@gmail.com>
2023-02-12 15:07:11 +0900

    print Hello World and make annotation

---
+++ 00_HelloWorld.py
@@ -0,0 +1,15 @@
+# 00 Hello World!
+# 출력문: print()
+
+# 한줄 주석
+# 단축키: Ctrl + /
+
+''' 여러줄 주석
+단축키: Alt + Shift + A '''
+
+print("Hello World!")   # Hello World! 출력
+print("안녕!")  # 안녕! 출력
+
+# 파이썬은 세미콜론이 없어도 됨
+# 한 줄로 이어서 입력 시 세미콜론을 쓰긴 함
+# print()는 자동 개행이 됨
